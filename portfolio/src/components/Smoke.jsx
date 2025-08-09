7369e88c173899788bb5bddce2d3375d258c63ad
luka chikashvili <lukachikashvili2020@gmail.com>
2025-08-09 15:28:30 +0400

    add smoke effect

--- portfolio/src/components/Smoke.jsx
+++ portfolio/src/components/Smoke.jsx
@@ -1,6 +1,7 @@
 import { smokeFragment } from '@/shaders/smoke/smokeFragment'
 import { smokeVertex } from '@/shaders/smoke/smokeVertex'
 import { useGLTF, useTexture } from '@react-three/drei'
+import { useFrame } from '@react-three/fiber'
 import React, { useRef } from 'react'
 import * as THREE from 'three'
 
@@ -8,15 +9,23 @@ const Smoke = () => {
    
     // perlin noise
     const perlineTexture = useTexture('./perlin.png');
+    perlineTexture.wrapS = THREE.RepeatWrapping;
+    perlineTexture.wrapT = THREE.RepeatWrapping;
 
     const uniforms = useRef({
-        uPerlineTexture: { value: perlineTexture}
+        uPerlineTexture: { value: perlineTexture},
+        uTime: { value: 0}
+    });
+
+
+    useFrame(() => {
+        uniforms.current.uTime.value += 0.025;
     })
      
   return (
     <>
       <mesh position={[6.75, 17.3, 5.7]}>
-      <planeGeometry args = {[1, 5, 16, 64]} />
+      <planeGeometry args = {[0.5, 5, 16, 64]} />
       <shaderMaterial color = "red" wireframe = {false} 
       vertexShader={smokeVertex} fragmentShader={smokeFragment}
        side = {THREE.DoubleSide} transparent = {true} uniforms = {uniforms.current} />
--- portfolio/src/shaders/smoke/smokeFragment.js
+++ portfolio/src/shaders/smoke/smokeFragment.js
@@ -1,13 +1,26 @@
 export const smokeFragment = `
 varying vec2 vUv;
 uniform sampler2D uPerlineTexture;
+uniform float uTime;
 
 void main() {
-   
+    vec2 smokeUv = vUv;
+    smokeUv.x *= 0.5;
+    smokeUv.y *= 0.3;
+    smokeUv.y -= uTime * 0.03;
+     
+    float smoke = texture(uPerlineTexture, smokeUv).r;
 
-    float smoke = texture(uPerlineTexture, vUv).r;
+    smoke = smoothstep(0.4, 1.0, smoke);
 
-    gl_FragColor = vec4(1.0, 1.0, 1.0, smoke);
+    smoke *= smoothstep(0.0, 0.1, vUv.x);
+    smoke *= smoothstep(1.0, 0.9, vUv.x);
+    smoke *= smoothstep(0.0, 0.1, vUv.y);
+    smoke *= smoothstep(1.0, 0.4, vUv.y);
+
+    vec3 candleColor = vec3(1.0, 0.7, 0.2);
+
+    gl_FragColor = vec4(candleColor, smoke);
 
     #include <tonemapping_fragment>
     #include <colorspace_fragment>
