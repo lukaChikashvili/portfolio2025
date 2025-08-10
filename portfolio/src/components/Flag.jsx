c2d01f4852d6b14348a40d403f108733e7387fbc
luka chikashvili <lukachikashvili2020@gmail.com>
2025-08-10 18:57:49 +0400

    create reusable flag component

---
+++ portfolio/src/components/Flag.jsx
@@ -0,0 +1,41 @@
+import { flagFragment } from '@/shaders/flags/fragment'
+import { flagVertex } from '@/shaders/flags/vertex'
+import { useFrame } from '@react-three/fiber'
+import React, { useRef } from 'react'
+import * as THREE from 'three'
+import { useTexture } from '@react-three/drei'
+
+const Flag = ({ texturePath, position, rotation }) => {
+  const shaderRef = useRef()
+  const texture = useTexture(texturePath)
+
+  const uniforms = useRef({
+    uTime: { value: 0 },
+    uAmplitude: { value: 0.5 },
+    uFrequency: { value: new THREE.Vector2(1.85, 1.0) },
+    uSpeed: { value: 3.0 },
+    uTexture: { value: texture },
+  })
+
+  useFrame(({ clock }) => {
+    if (shaderRef.current) {
+      shaderRef.current.uniforms.uTime.value = clock.elapsedTime
+    }
+  })
+
+  return (
+    <mesh rotation={rotation} position={position}>
+      <planeGeometry args={[3, 10, 128, 128]} />
+      <shaderMaterial
+        ref={shaderRef}
+        wireframe={true}
+        side={THREE.DoubleSide}
+        vertexShader={flagVertex}
+        fragmentShader={flagFragment}
+        uniforms={uniforms.current}
+      />
+    </mesh>
+  )
+}
+
+export default Flag
--- portfolio/src/components/Menu.jsx
+++ portfolio/src/components/Menu.jsx
@@ -1,41 +1,25 @@
-import { flagFragment } from '@/shaders/flags/fragment'
-import { flagVertex } from '@/shaders/flags/vertex'
-import { useTexture } from '@react-three/drei';
-import { useFrame } from '@react-three/fiber';
+
 import React, { useRef } from 'react'
-import * as THREE from 'three'
+import Flag from './Flag';
 
 const Menu = () => {
 
-    const shaderRef = useRef();
-
-    const projectTexture = useTexture('./projects.png');
-
-
-    
-    const uniforms = useRef({
-        uTime: { value: 0 },
-        uAmplitude: { value: 0.5},
-        uFrequency:  {value: new THREE.Vector2(1.85, 1.0)},
-        uSpeed: { value: 3.0},
-        uTexture: { value: projectTexture}
-    });
-
-    useFrame(({ clock }) => {
-        if (shaderRef.current) {
-          shaderRef.current.uniforms.uTime.value = clock.elapsedTime
-        }
-      })
+ 
 
 
   return (
    <>
-     <mesh rotation = {[-0.2, -0.7, 0]} position={[-20, 10, 0]}>
-         <planeGeometry args = {[3, 10, 128, 128]} />
-         <shaderMaterial ref={shaderRef} wireframe = {true} 
-           side={THREE.DoubleSide}
-         vertexShader={flagVertex} fragmentShader={flagFragment} uniforms={uniforms.current} />
-     </mesh>
+        <Flag
+        texturePath='./projects.png'
+        position={[-20, 10, 0]}
+        rotation={[-0.2, -0.7, 0]}
+      />
+
+<Flag
+        texturePath='./about.png'
+        position={[10, 10, 20]}
+        rotation={[0, -0.9, 0]}
+      />
    </>
   )
 }
